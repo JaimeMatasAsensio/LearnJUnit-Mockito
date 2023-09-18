9a9fcdc8f216689d53012ef11767b31ef35cbf80
unknown <jaimematas88@gmail.com>
2023-09-18 20:43:25 +0200

    Seccion1 180923

---
+++ junit5_app/src/main/java/org/jmatasa/junit5_app/ejemplos/models/Cuenta.java
@@ -0,0 +1,29 @@
+package org.jmatasa.junit5_app.ejemplos.models;
+
+import java.math.BigDecimal;
+
+public class Cuenta {
+
+    private BigDecimal saldo;
+    private String persona;
+
+    //Get&set para Persona
+    public String getPersona() {
+        return persona;
+    }
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+
+    //Get&Set para Saldo
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+
+
+}
